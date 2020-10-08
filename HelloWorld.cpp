d8783313a236ba8a40b37212807df7b14c61a9d9
Sahil6112003 <72222593+Sahil6112003@users.noreply.github.com>
2020-10-08 13:48:07 +0530

    added code using oop in cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,3 +6,19 @@ int main()
     cout <<"\nHello World"<< endl;
     return 0;
 }
+
+
+//======================================================
+
+#include <iostream>
+using namespace std;
+
+class helloworld{
+    helloworld(){
+        printf("Hello World!!");
+    }
+};
+
+int main(){
+    helloworld obj= new helloworld();
+}
